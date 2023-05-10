aaabcfb4192d64b6fb59c297490de67be088fc9f
Pooja Chhillar <pooja.chhillar@w3villa.com>
2023-05-10 15:34:14 +0530

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,60 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy ]
+
+  # GET /articles or /articles.json
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1 or /articles/1.json
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # POST /articles or /articles.json
+  def create
+    @article = Article.new(article_params)
+      if @article.save
+        flash[:notice] = "Article was created Successfully ."
+        redirect_to @article 
+      else
+        render :new , status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /articles/1 or /articles/1.json
+  def update
+   if @article.update(article_params)
+flash[:notice]= "Article was updated Successfully"
+redirect_to @article
+else
+render 'edit',status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /articles/1 or /articles/1.json
+  def destroy
+@article = Article.find(params[:id])
+@article.destroy
+redirect_to articles_path
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+end
