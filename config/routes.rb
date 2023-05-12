1a6790477dd9063314eb5d1edf0ba28e7150ea10
Pooja Chhillar <124652695+w3villa-pooja-chhillar@users.noreply.github.com>
2023-05-12 12:23:57 +0530

    Revert "signup form"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :users , except: [:new]
+  resources :users
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'signup' , to: 'users#new'
-  post 'sigup' , to: 'users#create'
 end
