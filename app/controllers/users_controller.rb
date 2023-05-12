6f1fe3c5f2ac27cb751265249c04b10ddc887231
Pooja Chhillar <pooja.chhillar@w3villa.com>
2023-05-12 11:48:47 +0530

    signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,15 +22,11 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+    if @user.save
+        flash[:notice] = 'Welcome to the alpha blog, you have successfully signup'
+        redirect_to articles_path
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+        render :new , status: :unprocessable_entity
     end
   end
 
@@ -65,6 +61,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
